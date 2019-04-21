0745d4362d1d4f74262e99520127d1a711951035
Brandon Robert Register <henrytouchdown@Brandons-MacBook-Pro.local>
2019-04-21 10:57:31 -0400

    Inital Commit

---
+++ Leaderboard.php
@@ -0,0 +1,91 @@
+<?php include('Score_Request.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>MiBGa Leaderboard</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <style>
+@import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
+@import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+
+
+
+.body{
+  position: fixed;
+  overflow-y: scroll;
+  width: 100%;
+top: -20px;
+left: -20px;
+right: -40px;
+bottom: -40px;
+width: auto;
+height: auto;
+background-image: url(MIGBa_Background.gif);
+background-size: cover;
+-webkit-filter: blur(0px);
+}
+
+.header{
+position: absolute;
+top: calc(50% - 35px);
+left: calc(50% - 255px);
+
+}
+
+.header div{
+float: center;
+color: #000080;
+font-family: 'Exo', sans-serif;
+font-size: 40px;
+font-weight: 200;
+}
+
+.header div span{
+color: #F29E20 !important;
+}
+
+::-webkit-input-placeholder{
+ color: rgba(17,5,121,1);
+}
+
+::-moz-input-placeholder{
+ color: rgba(255,255,255,0.6);
+}
+
+table {
+color: rgba(17,5,121,1);
+}
+
+h1 {
+  font-size: 4em;
+  color: rgba(17,5,121,1);
+}
+
+</style>
+
+</head>
+<body>
+  <h1 align="center">- Top Ten -</h1>
+  <table style="width: 100%" align="center">
+    <tr>
+      <th>Rank</th>
+      <th>Score</th>
+      <th>Username</th>
+    </tr>
+    <?php
+    for ($i = 0; $i <= 10; $i++) {
+      echo "
+      <tr>
+        <td>" + $i + "</td>
+        <td>" + $scoreboard_user + "</td>
+        <td>" + $scoreboard_score + "</td>
+      </tr>";
+      ;
+    }
+    ?>
+  </table>
+  <!-- I'm not sure how I can populate that scoreboard here -->
+  <p><?php echo $scoreboard ?></p>
+</body>
+</html>
---
+++ Score_Request.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// connect to the database
+$db = mysqli_connect('localhost', 'MIGBa_DB_admin',
+'PenguinPie', 'MIBGa_DB');
+
+// initialize variables
+$scoreboard_user = array();
+$scoreboard_score = array();
+
+// get top 10 users in database, by score
+$scoreboard = mysqli_query("SELECT username,
+FROM MIBGa_DB
+ORDER BY score DESC
+LIMIT 10");
+
+// get top 10 scores in the database
+$scoreboard_score = mysqli_query("SELECT username,
+FROM MIBGa_DB
+ORDER BY score DESC
+LIMIT 10");
+
+?>
